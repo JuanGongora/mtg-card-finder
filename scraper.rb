5d60bf6d667389c17b44230d1f7a5fcbedaea19a
Juan Gongora <gongora.animations@gmail.com>
2017-02-16 21:27:04 +0000

    add initialize method to MTG class

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,15 @@ require 'pry'
 
 class MTG
   attr_accessor :card, :rarity, :market_price, :wholesale_price, :image
+  @@all_cards = []
 
+  #need to make instance methods from attr_accessor be associated with
+  #the values I scrape from the website, all instances will be recorded
+  #into class variable @@all_cards with the recorded paired method values. 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send("#{key}=", value)}
+    @@all_cards << self
+  end
 end
 
 class Scraper
