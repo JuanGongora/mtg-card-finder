75434c18e067a175468df0d1e5af092c81d365c6
Juan Gongora <gongora.animations@gmail.com>
2017-02-16 22:08:24 +0000

    add initial parsing into Scraper.scrape_data

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,4 @@
-require 'open-uri'
+# require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
@@ -8,7 +8,7 @@ class MTG
 
   #need to make instance methods from attr_accessor be associated with
   #the values I scrape from the website, all instances will be recorded
-  #into class variable @@all_cards with the recorded paired method values. 
+  #into class variable @@all_cards with the recorded paired method values.
   def initialize(attributes)
     attributes.each {|key, value| self.send("#{key}=", value)}
     @@all_cards << self
@@ -17,6 +17,16 @@ end
 
 class Scraper
 
+  #use HTTP request to website in the 'open' method with open-uri, then
+  #creates a nokogiri wrapped object inside our local variable 'doc'.
   def self.scrape_data
+    doc = Nokogiri::HTML(open("http://prices.tcgplayer.com/price-guide"))
+    #parses our nokogiri object for the css selector that defines our
+    #preliminary category queries.
+    doc.css("tbody").each do |row|
+    binding.pry
+    end
   end
 end
+
+Scraper.scrape_data
