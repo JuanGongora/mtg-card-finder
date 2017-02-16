1d48b3f501d2c55c1bd5e72703339c009b353b39
Juan Gongora <gongora.animations@gmail.com>
2017-02-16 21:15:42 +0000

    add MTG class w type attr, add Scraper class

---
+++ scraper.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class MTG
+  attr_accessor :card, :rarity, :market_price, :wholesale_price, :image
+
+end
+
+class Scraper
+
+  def self.scrape_data
+  end
+end
