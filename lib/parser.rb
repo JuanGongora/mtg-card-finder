389757a2fefdb094355d87b2657228ce225f9fe3
Juan Gongora <gongora.animations@gmail.com>
2017-03-11 04:25:02 +0000

    changed request in Mechanize scrape to Referer

--- lib/parser.rb
+++ lib/parser.rb
@@ -10,7 +10,7 @@ class Cards
     #I also want to have the site understand the request that I want to keep access to
     agent.pre_connect_hooks << lambda do |agent, request|
         agent.user_agent = "Ruby/#{RUBY_VERSION}"
-        request["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+        request["Referer"] = "http://www.mtgprice.com/quickList"
       end
         page = agent.get "http://www.mtgprice.com/taneLayout/mtg_price_tracker.jsp?period=DAILY"
         page.css(".card a")[0].text
