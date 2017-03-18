58312ce9969fab80a0ad86d9ad8f7a4606cdb00c
Juan Gongora <gongora.animations@gmail.com>
2017-03-18 22:29:10 +0000

    revising CardTable.reify_from_row(row) and CardTable.find(id)

--- Rakefile
+++ Rakefile
@@ -2,6 +2,8 @@ require_relative './lib/parser'
 
 def reload!
   load_all './lib'
+  load_all './db'
+  load_all './fixtures'
 end
 
 task :console do
--- lib/card_table.rb
+++ lib/card_table.rb
@@ -5,14 +5,14 @@ DB = {:conn => SQLite3::Database.new("db/cards.db")}
 
 class CardTable
 
-  attr_accessor :card, :sets, :market_price, :price_fluctuate, :image
+  attr_accessor :id, :card, :sets, :market_price, :price_fluctuate, :image
 
-def self.table_name
-  "#{self.to_s.downcase}s"
-end
+  def self.table_name
+    "#{self.to_s.downcase}s"
+  end
 
-def self.create_table
-  sql = <<-SQL
+  def self.create_table
+    sql = <<-SQL
         CREATE TABLE IF NOT EXISTS #{self.table_name} (
           id INTEGER PRIMARY KEY,
           card TEXT,
@@ -21,56 +21,60 @@ def self.create_table
           price_fluctuate TEXT,
           image TEXT
           )
-          SQL
+    SQL
 
-  DB[:conn].execute(sql)
-end
+    DB[:conn].execute(sql)
+  end
 
-def insert
-  sql = <<-SQL
+  def insert
+    sql = <<-SQL
         INSERT INTO #{self.class.table_name} (card, sets, market_price, price_fluctuate, image) VALUES (?,?,?,?,?)
-           SQL
+    SQL
 
-  DB[:conn].execute(sql, self.card, self.sets, self.market_price, self.price_fluctuate, self.image)
-end
+    DB[:conn].execute(sql, self.card, self.sets, self.market_price, self.price_fluctuate, self.image)
+  end
 
-def self.find(id)
-  sql = <<-SQL
-        FIND * FROM (self.class.table_name) WHERE id=(?)
-        SQL
+  def self.find(id)
+    sql = <<-SQL
+        SELECT * FROM #{self.table_name} WHERE id=(?)
+    SQL
 
-  row = DB[:conn].execute(sql, id)
-  self.reify_from_row(row.first)  #using first array method to return only the first nested array
-end
+    row = DB[:conn].execute(sql, id)
+    self.reify_from_row(row.first)
+    #using first array method to return only the first nested array
+    #that is taken from self.reify_from_row(row) which is the resulting id of the query
+  end
 
-#opposite of abstraction is reification i.e. I'm getting the raw data of these variables
-def self.reify_from_row(row.first)
-  self.new.tap do |card|
-    card.id = row[0]
-    card.card = row[1]
-    card.sets = row[2]
-    card.market_price = row[3]
-    card.price_fluctuate = row[4]
-    card.image = row[5]
+  #opposite of abstraction is reification i.e. I'm getting the raw data of these variables
+  def self.reify_from_row(row)
+    self.new.tap do |card|
+      card.id = row[0]
+      card.card = row[1]
+      card.sets = row[2]
+      card.market_price = row[3]
+      card.price_fluctuate = row[4]
+      card.image = row[5]
     end
-end
+  end
 
 end
 
 
 
-# CardTable.create_table
-#
-# first = CardTable.new
-#
-# first.card = "Falkenrath"
-#
-# first.sets = "Innistrad"
-#
-# first.market_price = 23
-#
-# first.price_fluctuate = "+26"
-#
-# first.image = "ugly looking fella"
-#
-# first.insert
+CardTable.create_table
+
+first = CardTable.new
+
+first.card = "Falkenrath"
+
+first.sets = "Innistrad"
+
+first.market_price = 23
+
+first.price_fluctuate = "+26"
+
+first.image = "ugly looking fella"
+
+first.insert
+
+puts CardTable.find(1)
