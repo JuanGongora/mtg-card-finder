6bf38639e0cbb1ec043d9358a96dbe2df6f7ebb3
Juan Gongora <gongora.animations@gmail.com>
2017-03-28 01:58:53 +0000

    CardTable.make_csv_file now fully operational

--- lib/card_table.rb
+++ lib/card_table.rb
@@ -20,11 +20,6 @@ class CardTable
     attr_accessor key
   end
 
-  # def self.make_text_file
-  #   local_fname = "cards.txt"
-  #   File.open(local_fname, "w"){|file| file.write(open("db/cards.db").read)}
-  # end
-
   def self.make_csv_file
     rows = DB[:conn].execute("SELECT * FROM #{self.table_name}") #collects everything in sql table
     fname = "cards.csv" #naming the csv file
@@ -32,7 +27,10 @@ class CardTable
     unless File.exists? fname
       File.open(fname, 'w') do |ofile| #opening the csv file to write data into
         ofile << col_names
-        rows.each_with_index {|value, index| ofile << "#{rows[index].compact.join(", ")} \n"} #pushing each array row as a newline into csv while removing nil values
+        rows.each_with_index do |value, index|
+          value.each { |find| find.sub!(", ", "_") if find.is_a?(String) } #iterates through the row's values to replace commas so as to avoid line breaking errors
+          ofile << "#{rows[index].compact.join(", ")} \n" #pushing each array row as a newline into csv while removing nil values
+        end
         sleep(1 + rand)
       end
     end
