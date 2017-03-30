f695b3591ba3a677757f8cc095e5c7da19c3c6a0
Juan Gongora <gongora.animations@gmail.com>
2017-03-30 19:41:10 +0000

    made separate table classes to replace CardTable

---
+++ lib/tables/modern_fall.rb
@@ -0,0 +1,24 @@
+class ModernFall
+  include Persistable::InstanceMethods
+  extend Persistable::ClassMethods
+
+  #metaprogramming the hash to convert keys to attr_accessor's and also for inserting the values to the sql strings
+  ATTRS = {
+    :id => "INTEGER PRIMARY KEY",
+    :card => "TEXT",
+    :sets => "TEXT",
+    :market_price => "INTEGER",
+    :price_fluctuate => "TEXT",
+    :image => "TEXT"
+  }
+
+  #reader that can be accessed by Persistable module to know the unique class's constant
+  def self.attributes
+    ATTRS
+  end
+
+  #abstracting the collection of keys into attributes
+  self.attributes.keys.each do |key|
+    attr_accessor key
+  end
+end
--- lib/tables/standard_fall.rb
+++ lib/tables/standard_fall.rb
@@ -1,4 +1,4 @@
-class CardTable
+class StandardFall
   include Persistable::InstanceMethods
   extend Persistable::ClassMethods
 
