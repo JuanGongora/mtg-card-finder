3fe8888b5ccf4dc6f44763f9626173e5cc80bd71
Juan Gongora <gongora.animations@gmail.com>
2017-03-31 00:53:18 +0000

    revised Parser.scrape_cards & Parser.select_format, add CLI.set_choosing

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,20 +10,17 @@ class CLI
     puts "#{"[3]".fg COLORS[3]} Standard: #{"crashing".fg COLORS[6]} cards today"
     puts "#{"[4]".fg COLORS[3]} Modern: #{"crashing".fg COLORS[6]} cards today"
     puts "-------------------------------------------------"
-    self.check_input
-    Parser.scrape_cards
-    MTG.all
-    # MTG.call
-    puts ""
-    puts "-------------------------------------------------"
-    puts ""
+    self.set_choosing
     puts "Would you like to?"
     puts "#{"[1]".fg COLORS[3]} search for a different format's market?"
     puts "#{"[2]".fg COLORS[3]} save the current card search listing into a CSV file?"
     puts "#{"[3]".fg COLORS[3]} purchase one of the queried cards in the open market?"
-
-    input = gets.strip.to_i
-    CardTable.buy_link(input)
+    puts "-------------------------------------------------"
+    self.set_choosing
+    self.set_choosing
+    self.set_choosing
+    # input = gets.strip.to_i
+    # CardTable.buy_link(input)
 
   end
 
@@ -32,21 +29,14 @@ class CLI
 #have four different sql tables to store the different info of the market trend, that way the content
 #is not re-scraped from the site which would in turn only exacerbate the internet bandwith
 
-
-  # def self.check_input
-  #   sleep(1)
-  #   puts "Please type out the number of the set you would like to see from above..."
-  #   input = gets.strip.to_i
-  #   if Set.set_amount.include?(input)
-  #     puts "You chose Set #{input}: #{Set.set_name(input)}."
-  #     sleep(1)
-  #     Scraper.scrape_cards(Set.set_name_url(input))
-  #     MTG.all
-  #   else
-  #     puts "invalid option"
-  #     self.check_input
-  #   end
-  # end
+  def self.set_choosing
+    self.check_input
+    Parser.scrape_cards
+    MTG.all
+    puts ""
+    puts "-------------------------------------------------"
+    puts ""
+  end
 
   def self.check_input
     sleep(1)
--- lib/parser.rb
+++ lib/parser.rb
@@ -16,8 +16,10 @@ class Parser
   # end
 
   def self.scrape_cards
-CardTable.remove_table
-    CardTable.create_table
+    #Klass.remove_table
+    @@overall_format_options[4].call
+    #Klass.create_table
+    @@overall_format_options[5].call
     doc = Nokogiri::HTML(open("./fixtures/test.html"))
     self.card_counter
     doc.css(@@overall_format_options[0]).each do |row|
@@ -32,9 +34,11 @@ CardTable.remove_table
           # image: Nokogiri::HTML(open("http://www.mtgprice.com#{row.css(".card a").attribute("href").value}")).css(".card-img img").attribute("src").value
           # ^^ had to go another level deep to access a better quality image from its full product listing
       })
-      CardTable.create(hash)
+      #since a stored method in an array can't have a locally passed argument I compromised by just having the class name passed instead
+      @@overall_format_options[6].create(hash)
     end
-    CardTable.make_csv_file
+    #Klass.make_csv_file
+    @@overall_format_options[7].call
   end
 
   def self.card_counter
@@ -52,13 +56,14 @@ CardTable.remove_table
     input = gets.strip.to_i
     case input
       when 1
-        @@overall_format_options = ["#top50Standard tr", "top", "Standard", "#{"gainers".fg COLORS[4]}"]
+        #the methods at the end of these arrays are stored references that can be called externally with the .call method #=>  http://stackoverflow.com/questions/13948910/ruby-methods-as-array-elements-how-do-they-work
+        @@overall_format_options = ["#top50Standard tr", "top", "Standard", "#{"gainers".fg COLORS[4]}", StandardRise.method(:remove_table), StandardRise.method(:create_table), StandardRise, StandardRise.method(:make_csv_file)]
       when 2
-        @@overall_format_options = ["#top50Modern tr", "top", "Modern", "#{"gainers".fg COLORS[4]}"]
+        @@overall_format_options = ["#top50Modern tr", "top", "Modern", "#{"gainers".fg COLORS[4]}", ModernRise.method(:remove_table), ModernRise.method(:create_table), ModernRise, ModernRise.method(:make_csv_file)]
       when 3
-        @@overall_format_options = ["#bottom50Standard tr", "bottom", "Standard", "#{"crashers".fg COLORS[6]}"]
+        @@overall_format_options = ["#bottom50Standard tr", "bottom", "Standard", "#{"crashers".fg COLORS[6]}", StandardFall.method(:remove_table), StandardFall.method(:create_table), StandardFall, StandardFall.method(:make_csv_file)]
       when 4
-        @@overall_format_options = ["#bottom50Modern tr", "bottom", "Modern", "#{"crashers".fg COLORS[6]}"]
+        @@overall_format_options = ["#bottom50Modern tr", "bottom", "Modern", "#{"crashers".fg COLORS[6]}", ModernFall.method(:remove_table), ModernFall.method(:create_table), ModernFall, ModernFall.method(:make_csv_file)]
       else
         CLI.check_input
     end
@@ -72,7 +77,7 @@ CardTable.remove_table
   def self.update_query_info
     current = Nokogiri::HTML(open("./fixtures/test.html")).css(".span6 h3")[0].text.split.join(" ").gsub!("Updated:", "")[0..10]
     if current != @@time_review[0..10]
-      CardTable.remove_table
-end
+      @@overall_format_options[4].call
+    end
   end
 end
